1cd1b9e36012c97853797b51fe708e83eb7c7c53
Yaswanth Haridasu <yaswanthharidasu48@gmail.com>
2024-12-21 16:21:09 +0530

    fix: no of shortest paths using dijkstra's

--- graphs/dijkstras.py
+++ graphs/dijkstras.py
@@ -1,4 +1,5 @@
 import heapq
+MOD = int(1e9+7)
 
 def dijsktras(n, edges, start):
     graph = [[] for _ in range(n+1)]
@@ -9,7 +10,7 @@ def dijsktras(n, edges, start):
     distances[start] = 0
     shortest = [(0, start)]
 
-    while len(shortest) != 0:
+    while shortest:
         currDistance, u = heapq.heappop(shortest)
         # To avoid reiterating of neighbours of node u (same node can present two times in the heap)
         if currDistance > distances[u]:
@@ -18,3 +19,27 @@ def dijsktras(n, edges, start):
             if currDistance+wt < distances[v]:
                 distances[v] = currDistance+wt
                 heapq.heappush(shortest, (distances[v], v))
+
+def noOfShortestPaths(n, edges, start):
+    graph = [[] for _ in range(n+1)]
+    for u,v,wt in edges:
+        graph[u].append([v, wt])
+
+    distances = [float('inf') for _ in range(n+1)]
+    paths = [0 for _ in range(n+1)]
+    distances[start] = 0
+    paths[0] = 1
+    shortest = [(0, start)]
+
+    while shortest:
+        currDistance, u = heapq.heappop(shortest)
+        if currDistance > distances[u]:
+            continue
+
+        for v, wt in graph[u]:
+            if currDistance+wt < distances[v]:
+                distances[v] = currDistance+wt
+                paths[v] = paths[u]%MOD
+                heapq.heappush(shortest, (distances[v], v))
+            elif currDistance+wt == distances[v]:
+                paths[v] = (paths[v]+paths[u])%MOD
